ddb832a113f8d275eccb713dd45874697053c45d
GuidoSchmidt <guido.schmidt.2912@gmail.com>
2014-05-14 21:52:34 +0200

    some changeings

--- nimple/index.php
+++ nimple/index.php
@@ -1,38 +1,66 @@
 <?php
-  require('../inc/header.php');
+	require('../inc/header.php');
 ?>
 
-      <header class="page-header">
-        <div class="container">
-            <h1>Nimple</h1>
-            <h2>Networking is simple.</h2>
-            <div class="badges">
-              <div class="badge">
-                <!-- Play Store badge -->
-                <a href="https://play.google.com/store/apps/details?id=de.nimple">
-                  <img alt="Jetzt bei Google Play"
-                       src="https://developer.android.com/images/brand/de_generic_rgb_wo_45.png" />
-                </a>
-              </div>
-              <div class="badge">
-                <!-- App Store badge -->
-                <a href="https://itunes.apple.com/de/app/nimple/id847059589">
-                  <img alt="Laden im App Store"
-                       src="../img/badges/appstore/Download_on_the_App_Store_Badge_DE_135x40.svg">
-                </a>
-              </div>
-            </div>
+<header class="page-header">
 
-            <div class="frame_Nexus5">
-              <div class="frameScreenshot">
-                <img src="../img/screenshots/android/edit-fields.png" alt="Nimple Android Kontaktdaten Eingabe">
-              </div>
-            </div>
+	<div class="container">
+		<!-- left side -->
+		<div class="left-side">
+			<div class="media">
+				<div class="pull-left">
+					<img class="media-object" id="logo" src="../img/logos/nimple.png" alt="Nimple Logo">
+				</div>
+				<div class="media-body">
+					<h1 class="media-heading">Nimple</h1>
+					<h2>Networking is simple.</h2>
+				</div>
+			</div>
 
-            <div class="frame_iPhone">
-              <div class="frameScreenshot">
-                <img src="../img/screenshots/iOS/contacts.png" alt="Nimple iOS Kontakte">
-              </div>
-            </div>
-        </div>
-      </header>
+			<div class="badges">
+				<div class="badge">
+					<!-- Play Store badge -->
+					<a href="https://play.google.com/store/apps/details?id=de.nimple">
+						<img alt="Jetzt bei Google Play"
+								 src="../img/badges/playstore/de_generic_rgb_wo_45.png"/>
+					</a>
+				</div>
+				<div class="badge">
+					<!-- App Store badge -->
+					<a href="https://itunes.apple.com/de/app/nimple/id847059589">
+						<img alt="Laden im App Store"
+								 src="../img/badges/appstore/Download_on_the_App_Store_Badge_DE_135x40.svg">
+					</a>
+				</div>
+			</div>
+
+			<section>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</section>
+
+		</div>
+
+		<!-- right side -->
+		<div class="rigth-side">
+			<div class="frame_Nexus5">
+				<div class="frameScreenshot">
+					<img src="../img/screenshots/android/edit-fields.png" alt="Nimple Android Kontaktdaten Eingabe">
+				</div>
+			</div>
+
+			<div class="frame_iPhone">
+				<div class="frameScreenshot">
+					<img src="../img/screenshots/iOS/contacts.png" alt="Nimple iOS Kontakte">
+				</div>
+			</div>
+		</div>
+
+	<!-- container end -->
+	</div>
+
+</header>
