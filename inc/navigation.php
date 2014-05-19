ed91a94a3311ce632dbc6e47330e5683cf9f8c20
GuidoSchmidt <guido.schmidt.2912@gmail.com>
2014-05-20 00:00:42 +0200

    fixed mobile version

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,8 +1,6 @@
-
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
   <div class="container-fluid">
 
-    <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
@@ -10,9 +8,9 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+      <a class="navbar-brand" href="#"></a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="../nimple/">Übersicht</a></li>
@@ -23,6 +21,6 @@
         <li><a href="../imprint/">Impressum</a></li>
       </ul>
 
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+    </div>
+  </div>
 </nav>
