37fa926e21aadd20c8b8e9eeaf4ea8354cf98b13
GuidoSchmidt <guido.schmidt.2912@gmail.com>
2014-06-27 14:52:41 +0200

    improved press section

--- press/index.php
+++ press/index.php
@@ -6,7 +6,7 @@
 	<h1>Presse</h1>
 	<h2>Nimple in den Medien</h2>
 
-	<div class="articles">
+	<div id="articles">
 	</div>
 
 </div>
@@ -15,29 +15,38 @@
   require('../inc/footer.php');
 ?>
 
+<script id="article-template" type="text/template">
+<div class="article">
+	<h1>{{title}}</h1>
+	<p>
+		{{text}}
+	</p>
+	<a href="{{link}}">{{link-text}}</a>
+	<hr>
+</div>
+</script>
+
 <script>
-$(function () {
-	function renderTemplate(view){
-		$.get('./template-article.mst', function(template) {
-			var rendered = Mustache.render(template, view);
-			$('.articles').append(rendered);
-			$('.articles div').velocity("fadeIn", {delay: 200, opacity: 1});
-		});
+
+
+	var animated = false;
+	var articles = [];
+
+	function displayArticles(articles) {
+		for(var i=0; i < articles.length; i++) {
+			renderTemplate(i, articles.length, articles[i]);
+		};
 	}
 
-	var articles;
-	$.getJSON( "articles.json", { name: "John", time: "2pm" } )
-		.done(function( json ) {
-			//console.log( "JSON Data: " + json.articles );
-			articles = json.articles;
-			$.each(articles, function(key, value) {
-				renderTemplate(value);
-			});
-		})
-		.fail(function( jqxhr, textStatus, error ) {
-			var err = textStatus + ", " + error;
-			//console.log( "Request Failed: " + err );
+	$.getJSON( "articles.json", function( data ) {
+		var items = [];
+		$.each( data.articles, function( key, value ) {
+			var template = $('#article-template').html();
+			var html = Mustache.to_html(template, value);
+			$('#articles').append(html);
 		});
-});
+		$('.article').velocity('transition.slideRightIn', {delay: 800, stagger: 500});
+	});
+
 
 </script>
